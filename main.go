cb2442e8d400aef109eba687141d7a9409a93f6b
VedDev <vedantyaduvanshi@gmail.com>
2022-05-28 05:45:56 +0530

    404 PAGE added

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1> Welcome to awesome Site </h1>")
-	} else if r.URL.Path == "/contact" { 
-		fmt.Fprint(w, "To get in touch please Send an email to <a 
-		href=\"mailto:support@lenslocked.com\">support@lenslocked.com<a/a>.")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch , Send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1> Cant find page fk u </h1>")
 	}
 
 }
