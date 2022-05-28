f2bb6c32432b1c6140d5bf4ffce5666725bb503c
VedDev <vedantyaduvanshi@gmail.com>
2022-05-28 10:59:32 +0530

    h

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1> Welcome to awesome Site </h1>")
-	} else if r.URL.Path == "/contact" {
+	} else if r.URL.Path == "/contact" || r.URL.Path == "/contact/" {
 		fmt.Fprint(w, "To get in touch , Send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
@@ -19,6 +21,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handlerFunc)
+	r.HandleFunc("/contact", handlerFunc)
+	http.ListenAndServe(":3000", r)
 }
